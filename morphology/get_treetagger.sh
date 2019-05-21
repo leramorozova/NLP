786d3fdbae8d1059b225a8dd5c4bd5778d1e8530
valeria <tito_alba@mail.ru>
2019-05-21 22:52:58 +0300

    add training

--- morphology/get_treetagger.sh
+++ morphology/get_treetagger.sh
@@ -5,4 +5,4 @@ wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/install-tagger
 wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/russian.par.gz
 sh install-tagger.sh
 !echo 'Я хочу съесть яблоко!' | cmd/tree-tagger-russian 
-rm -rf annot.opcorpora.no_ambig_strict.xml.zip russian.par.gz  tagger-scripts.tar.gz tree-tagger-linux-3.2.2.tar.gz
+rm -rf annot.opcorpora.no_ambig_strict.xml.zip russian.par.gz tagger-scripts.tar.gz tree-tagger-linux-3.2.2.tar.gz install-tagger.sh
